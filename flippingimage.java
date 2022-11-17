9c14663a277213501fb4bb7166b7c3b89fccd844
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2022-11-18 01:53:42 +0530

    flippingimage modified

--- flippingimage.java
+++ flippingimage.java
@@ -11,23 +11,37 @@ public class flippingimage {
     }
     static int[][] flipAndInvertImage(int[][] image) {
         int n= image.length;
-        int[][] ans = new int[n][n];
-        //flip
-        for (int i = 0; i <n; i++) {
-            for (int j = 0; j <n; j++) {
-                ans[i][j]=image[i][(n-1)-j];
-            }
-        }
-        //invert
-        for (int i = 0; i <n; i++) {
-            for (int j = 0; j <n; j++) {
-                if(ans[i][j]==0){
-                    ans[i][j]=1;
-                }
-                else ans[i][j]=0;
+
+//        int[][] ans = new int[n][n];
+//        //flip
+//        for (int i = 0; i <n; i++) {
+//            for (int j = 0; j <n; j++) {
+//                ans[i][j]=image[i][(n-1)-j];
+//            }
+//        }
+
+        //or
+        //flip but inner loop will run for n/2 times, coz we are swapping not initializing like before
+        int temp;
+        for (int i = 0; i < n; i++) {
+            //directly flip and invert 1 and 0
+            for (int j = 0; j < (n+1)/2; j++) {
+                temp=image[i][j];
+                image[i][j]=1-image[i][(n-1)-j];
+                image[i][(n-1)-j]=1-temp;
             }
         }
 
-        return ans;
+        //invert
+//        for (int i = 0; i <n; i++) {
+//            for (int j = 0; j <n; j++) {
+//                if(ans[i][j]==0){
+//                    ans[i][j]=1;
+//                }
+//                else ans[i][j]=0;
+//            }
+//        }
+
+        return image;
     }
 }
