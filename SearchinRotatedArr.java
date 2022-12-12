378d7bb7dad8687f71ca3203b837ca0b67dcfff6
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2022-12-12 20:27:17 +0530

    Search in Rotated Array modified

--- SearchinRotatedArr.java
+++ SearchinRotatedArr.java
@@ -37,6 +37,37 @@ public class SearchinRotatedArr {
         }
         return -1;
     }
+    //in case of duplicate elements
+    static int findPivotWithDuplicates(int[] arr){
+        int start=0;
+        int end = arr.length-1;
+        while (start<=end){
+            int mid = start+(end-start)/2;
+            if (mid>start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            if (mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if (arr[mid]==arr[start] && arr[mid]==arr[end]){
+                if (arr[start]>arr[start+1]){
+                    return start;
+                }
+                start++;
+                if (arr[end]<arr[end-1]){
+                    return end-1;
+                }
+                end--;
+            }
+            else if (arr[start]<arr[mid] || arr[start]==arr[mid] && arr[mid]>arr[end]){
+                start = mid+1;
+            }
+            else {
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
     static int binary_search(int[] arr, int target,int start, int end) {
         while (start<=end){
             int mid = start+(end-start)/2;
