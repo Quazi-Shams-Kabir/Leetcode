ed88d753629f1a9b620fea3f92c41d1fa5a395d6
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2023-01-07 12:17:11 +0530

    FIndAllDisappeared added

---
+++ FIndAllDisappeared.java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+//Google
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FIndAllDisappeared {
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        System.out.println(findDisappearedNumbers(nums));
+    }
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int i=0;
+        //first we will sort the array
+        while (i<nums.length){
+            int correct=nums[i]-1;
+            if (nums[i]!=nums[correct]){
+                swap(nums,i,correct);
+            }
+            else i++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        //search the missing elements and add it to list
+        for (int index = 0; index <nums.length ; index++) {
+            if (nums[index] != index+1){
+                ans.add(index+1);
+            }
+        }
+        return ans;
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
