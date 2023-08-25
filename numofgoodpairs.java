2ee5f4266ba1df222320bcb1d101104ed2685517
Quazi Shams Kabir <quaziraj12@gmail.com>
2023-08-25 09:29:46 +0530

    numofgoodpairs optimised code (hashmap)

--- numofgoodpairs.java
+++ numofgoodpairs.java
@@ -1,5 +1,7 @@
 //https://leetcode.com/problems/number-of-good-pairs/
 
+import java.util.HashMap;
+
 public class numofgoodpairs {
     public static void main(String[] args) {
         int[] nums = {1,2,3,1,1,3};
@@ -16,6 +18,7 @@ public class numofgoodpairs {
 //        }
 
 //        or
+        /*
         int[] count = new int[102];
         for(int i: nums){
              count[i]++;
@@ -24,5 +27,19 @@ public class numofgoodpairs {
            ans += ((i) * (i-1))/2;
         }
         return ans;
+         */
+
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int count = 0;
+        for(int i : nums){
+            if(hm.containsKey(i)){
+                count+=hm.get(i);
+                hm.put(i, hm.get(i)+1);
+            }
+            else{
+                hm.put(i,1);
+            }
+        }
+        return count;
     }
 }
